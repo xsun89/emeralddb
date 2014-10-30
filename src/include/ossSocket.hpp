354605be4a5252db58df821b60f9ec9985aa0c5d
xsun89 <sunxin89@gmail.com>
2014-10-30 13:45:17 -0700

    using clion

--- src/include/ossSocket.hpp
+++ src/include/ossSocket.hpp
@@ -28,5 +28,31 @@
 #include <string.h>
 #define SOCKET_GETLASTERROR errno
 
+//by default 10ms timout
+#define OSS_SOCKET_DFT_TIMEOUT 10000
+
+//max hostname
+#define OSS_MAX_HOSTNAME NI_MAXHOST
+#define OSS_MAX_SERVICENAME NI_MAXSERV
+
+class _ossSocket
+{
+private:
+    int _fd;
+    socket_t addressLen;
+    socket_t _peerAddressLen;
+    struct sockaddr_in _sockAddress;
+    struct sockaddr_in _peerAddress;
+
+    bool _init;
+    int _timeout;
+protected:
+    unsigned int _getPort( sockaddr_in *addr);
+    int _getAddress(sockaddr_in *addr, char *pAddress, unsigned int length);
+public:
+    
+};
+
+typedef class _ossSocket ossSocket ;
 
 #endif
